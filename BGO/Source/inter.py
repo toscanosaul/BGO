3e669ead50904c7b711964109b9a9189fd97303e
Saul Toscano <toscano.saul@gmail.com>
2016-02-20 22:08:32 -0500

    ,

--- BGO/Source/inter.py
+++ BGO/Source/inter.py
@@ -71,17 +71,19 @@ class objective:
         self.estimationObjective=estimationObjective
 
 class Miscellaneous:
-    def __init__(self,randomSeed,parallel,create=True,nF=0,tP=0,ALG="SBO",prefix=""):
+    def __init__(self,randomSeed,parallel,create=True,nF=0,tP=0,
+                 ALG="SBO",prefix=""):
         """
         Args:
-            -randomSeed: Random seed used to run the problem. Only needed for the
-                         name of the files with the results.
-            -parallel: True if we want to run the multistart gradient ascent algorithm
-                       and train the kernel of the GP in parallel; otherwise, it's false. 
+            -randomSeed: Random seed used to run the problem.
+                Only needed for the name of the files with the results.
+            -parallel: True if we want to run the multistart gradient
+                ascent algorithm and train the kernel of the GP in parallel;
+                otherwise, it's false. 
             -folder: Path where the files with the results are saved. 
-            -create: True if we want to create new files for the results; it's false
-                     otherwise. If we want to add more results to our previos results,
-                     this variable should be false.
+            -create: True if we want to create new files for the results;
+                     it's false otherwise. If we want to add more results
+                     to our previos results, this variable should be false.
             -nF: Number of samples to estimate the information source
             -tP: Number of training points
             -ALG: Algorithm that is used
@@ -90,8 +92,9 @@ class Miscellaneous:
         self.rs=randomSeed
         self.parallel=parallel
         
-
-        nameDirectory=prefix+"Results"+'%d'%nF+"AveragingSamples"+'%d'%tP+"TrainingPoints"
+        
+        tempDir="Results"+'%d'%nF+"AveragingSamples"+'%d'%tP+"TrainingPoints"
+        nameDirectory=prefix+tempDir
         folder=os.path.join(nameDirectory,ALG)
         self.folder=folder
         self.create=create
@@ -107,87 +110,87 @@ class opt:
         Args:
         -numberParallel: Number of starting points for the multistart gradient
                          ascent algorithm.
-        -dimXsteepestVn: Dimension of x when VOI is optimized. We may want to reduce
-                         the dimension of the original problem.
-        -dimXsteepestAn: Dimension of x when a_{n} is optimized. We may want to reduce
-                         the dimension of the original problem.
-        -transformationDomainXVn: Transforms the point x given by the steepest ascent
-                                method to the right domain of x.
-                                Its arugment its:
-                                    -x: The point to be transformed
-        -transformationDomainXAn: Transforms the point x given by the steepest ascent
-                                method to the right domain of x.
-                                Its arugment its:
-                                    -x: The point to be transformed
-        -transformationDomainW: Transforms the point w given by the steepest ascent
-                                method to the right domain of w.
-                                Its argument is:
-                                  -w: The point to be tranformed.
-        -projectGradient: Project a point x to the domain of the problem at each step
-                          of the gradient ascent method if needed.
-                          Its argument is:
-                            -x: The point that is projected.
-        -functionGradientAscentVn: Function used for the gradient ascent method. It
-                                   evaluates the VOI, when grad and onlyGradient are
-                                   False; it evaluates the VOI and computes its
-                                   derivative when grad is True and onlyGradient is False,
-                                   and computes only its gradient when gradient and
-                                   onlyGradient are both True.
-                                   Its arguments are:
-                                    -x: VOI is evaluated at x.
-                                    -grad: True if we want to compute the gradient;
-                                           False otherwise.
-                                    -i: Iteration of the SBO algorithm.
-                                    -L: Cholesky decomposition of the matrix A,
-                                        where A is the covariance matrix of the
-                                        past obsevations (x,w).
-                                    -Bfunc: Computes B(x,XW)=\int\Sigma_{0}(x,w,
-                                                     XW[0:n1],XW[n1:n1+n2])dp(w).
-                                    -temp2: temp2=inv(L)*B.T, where B is a matrix
-                                            such that B(i,j) is
-                                            \int\Sigma_{0}(x_{i},w,x_{j},w_{j})dp(w)
-                                            where points x_{p} is a point of the
-                                            discretization of the space of x; and
-                                            (x_{j},w_{j}) is a past observation.
-                                    -a: Vector of the means of the GP on G. The
-                                        means are evaluated on the
-                                        discretization of the space of x.
-                                    -VOI: VOI object
-                                    -kern: kernel
-                                    -XW: Past observations
-                                    -scratch: matrix where scratch[i,:] is the
-                                              solution of the linear system
-                                              Ly=B[j,:].transpose()
-                                    -onlyGradient: True if we only want to compute
-                                                    the gradient; False otherwise.
-        -functionGradientAscentAn: Function used for the gradient ascent method. It evaluates
-                                   a_{n}, when grad and onlyGradient are False; it evaluates
-                                   a_{n} and computes its derivative when grad is True and
-                                   onlyGradient is False, and computes only its gradient when
-                                   gradient and onlyGradient are both True.
-                                   Its arguments are:
-                                    x: a_{i} is evaluated at x.
-                                    grad: True if we want to compute the gradient; False
-                                          otherwise.
-                                    i: Iteration of the SBO algorithm.
-                                    L: Cholesky decomposition of the matrix A, where A is
-                                       the covariance matrix of the past obsevations (x,w).
-                                    dataObj: Data object.
-                                    stat: Statistical object.
-                                    onlyGradient: True if we only want to compute the gradient;
-                                                  False otherwise.
-                                    logproductExpectations: Vector with the logarithm of the
-                                                            product of the expectations of
-                                                            np.exp(-alpha2[j]*((z-W[i,j])**2))
-                                                            where W[i,:] is a point in the history.
-                                                            Only when SK is used.
-        -functionConditionOpt: Gives the stopping rule for the steepest ascent method, e.g. the
-                               function could be the Euclidean norm.
-                               Its arguments is:
-                                -x: Point where the condition is evaluated.
+        -dimXsteepestVn: Dimension of x when VOI is optimized.
+            We may want to reduce the dimension of the original problem.
+        -dimXsteepestAn: Dimension of x when a_{n} is optimized. We may want
+            to reduce the dimension of the original problem.
+        -transformationDomainXVn: Transforms the point x given by the steepest
+            ascent method to the right domain of x.
+            Its arugment is:
+                -x: The point to be transformed
+        -transformationDomainXAn: Transforms the point x given by the steepest
+                ascent method to the right domain of x.
+                Its arugment is:
+                    -x: The point to be transformed
+        -transformationDomainW: Transforms the point w given by the steepest
+            ascent method to the right domain of w.
+            Its argument is:
+                -w: The point to be tranformed.
+        -projectGradient: Project a point x to the domain of the problem at
+            each step of the gradient ascent method if needed.
+            Its argument is:
+                -x: The point that is projected.
+        -functionGradientAscentVn: Function used for the gradient ascent method.
+            It evaluates the VOI, when grad and onlyGradient are False;
+            it evaluates the VOI and computes its derivative when grad is True
+            and onlyGradient is False, and computes only its gradient when
+            gradient and onlyGradient are both True.
+            Its arguments are:
+                -x: VOI is evaluated at x.
+                -grad: True if we want to compute the gradient;
+                       False otherwise.
+                -i: Iteration of the SBO algorithm.
+                -L: Cholesky decomposition of the matrix A,
+                    where A is the covariance matrix of the
+                    past obsevations (x,w).
+                -Bfunc: Computes B(x,XW)=\int\Sigma_{0}(x,w,
+                                 XW[0:n1],XW[n1:n1+n2])dp(w).
+                -temp2: temp2=inv(L)*B.T, where B is a matrix
+                        such that B(i,j) is
+                        \int\Sigma_{0}(x_{i},w,x_{j},w_{j})dp(w)
+                        where points x_{p} is a point of the
+                        discretization of the space of x; and
+                        (x_{j},w_{j}) is a past observation.
+                -a: Vector of the means of the GP on G. The
+                    means are evaluated on the
+                    discretization of the space of x.
+                -VOI: VOI object
+                -kern: kernel
+                -XW: Past observations
+                -scratch: matrix where scratch[i,:] is the
+                          solution of the linear system
+                          Ly=B[j,:].transpose()
+                -onlyGradient: True if we only want to compute
+                                the gradient; False otherwise.
+        -functionGradientAscentAn: Function used for the gradient ascent method.
+            It evaluates a_{n}, when grad and onlyGradient are False;
+            it evaluates a_{n} and computes its derivative when grad is True and
+            onlyGradient is False, and computes only its gradient when gradient
+            and onlyGradient are both True.
+            Its arguments are:
+                x: a_{i} is evaluated at x.
+                grad: True if we want to compute the gradient; False
+                      otherwise.
+                i: Iteration of the SBO algorithm.
+                L: Cholesky decomposition of the matrix A, where A is
+                   the covariance matrix of the past obsevations (x,w).
+                dataObj: Data object.
+                stat: Statistical object.
+                onlyGradient: True if we only want to compute the gradient;
+                              False otherwise.
+                logproductExpectations: Vector with the logarithm of the
+                                        product of the expectations of
+                                        np.exp(-alpha2[j]*((z-W[i,j])**2))
+                                        where W[i,:] is a point in the history.
+                                        Only when SK is used.
+        -functionConditionOpt: Gives the stopping rule for the steepest ascent
+            method, e.g. the function could be the Euclidean norm.
+            Its arguments is:
+                -x: Point where the condition is evaluated.
         -xtol: Tolerance of x for the convergence of the steepest ascent method.
         -cons: Constraints of the problem if slsqp is used. See
-               http://docs.scipy.org/doc/scipy-0.14.0/reference/tutorial/optimize.html#tutorial-sqlsp
+            http://docs.scipy.org/doc/scipy-0.14.0/reference/tutorial/
+                optimize.html#tutorial-sqlsp
         -MethodVn: "SLSQP" or "OptSteepestDescent".
         -MethodAn: "SLSQP" or "OptSteepestDescent".
         -ConsVn: Constraints for optimization of Vn (only SLSQP)
@@ -228,7 +231,8 @@ class data:
         temp=data(Xcopy,ycopy,varcopy)
         return temp
     
-    def getTrainingDataKG(self,trainingPoints,noisyG,numberSamplesForG,parallel):
+    def getTrainingDataKG(self,trainingPoints,noisyG,numberSamplesForG,
+                          parallel):
         Xtrain=self.Xhist
         yTrain=np.zeros([0,1])
         NoiseTrain=np.zeros(0)
@@ -253,7 +257,8 @@ class data:
         self.yHist=yTrain
         self.varHist=NoiseTrain
         
-    def getTrainingDataSBO(self,trainingPoints,noisyF,numberSamplesForF,parallel):
+    def getTrainingDataSBO(self,trainingPoints,noisyF,numberSamplesForF,
+                           parallel):
         XWtrain=self.Xhist
         yTrain=np.zeros([0,1])
         NoiseTrain=np.zeros(0)
@@ -262,7 +267,8 @@ class data:
             jobs = []
             pool = mp.Pool()
             for i in xrange(trainingPoints):
-                job = pool.apply_async(noisyF,(XWtrain[i:i+1,:],numberSamplesForF))
+                job = pool.apply_async(noisyF,(XWtrain[i:i+1,:],
+                                               numberSamplesForF))
                 jobs.append(job)
             pool.close()  
             pool.join()  
